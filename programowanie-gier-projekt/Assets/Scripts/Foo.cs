b9d1d255922c4d4c365ef57dd0ae0d6ab9190789
arkadiusz-cholewa <arkadiusz.cholewaa@gmail.com>
2019-10-27 11:26:33 +0100

    Added score in gabe over scene

---
+++ programowanie-gier-projekt/Assets/Scripts/Foo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class Foo : MonoBehaviour
+    {
+        private Text _theText;
+        void Start()
+        {
+            _theText = GetComponent<Text>();
+        }
+
+        void Update()
+        {
+            _theText.text = "" + ScoreManager.Score;
+        }
+    }
+}
