7f75ebe744b2dfb998de5fcc94732b6934257f41
arkadiusz-cholewa <arkadiusz.cholewaa@gmail.com>
2019-10-27 14:06:56 +0100

    Added scoretable

---
+++ programowanie-gier-projekt/Assets/Scripts/HighscoreTable.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System; //This allows the IComparable Interface
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighscoreTable : MonoBehaviour
+{
+    private Transform entryContainer;
+    private Transform entryTemplate;
+    private List<HighscoreEntry> highscoreEntryList;
+    private List<Transform> highscoreEntryTransformList;
+    private void Awake()
+    {
+        entryContainer = transform.Find("HightscoreEntryContainer");
+        entryTemplate = entryContainer.Find("HighscoreEntryTemplate");
+
+        entryTemplate.gameObject.SetActive(false);
+        AddHighscoreEntry(100000, "ANIA");
+
+        string jsonString = PlayerPrefs.GetString("highscoreTable");
+        var highscores = JsonUtility.FromJson<Highscores>(jsonString);
+        highscoreEntryList = highscores.highscoreEntryList;
+        highscoreEntryList.Sort();
+
+        highscoreEntryTransformList = new List<Transform>();
+        foreach (var highscoreEntry in highscoreEntryList)
+        {
+            CreateHighscoreEntryTransform(highscoreEntry, entryContainer, highscoreEntryTransformList);
+        }
+    }
+
+    private void CreateHighscoreEntryTransform(HighscoreEntry highscoreEntry, Transform container, List<Transform> transformList)
+    {
+        float templateHight = 15f;
+        Transform entryTransform = Instantiate(entryTemplate, container);
+        RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+        entryRectTransform.anchoredPosition = new Vector2(0, -templateHight * transformList.Count);
+        entryTransform.gameObject.SetActive(true);
+
+        int rank = transformList.Count + 1;
+        string rankString;
+
+        switch (rank)
+        {
+            default:
+                rankString = rank + "TH"; break;
+
+            case 1: rankString = "1ST"; break;
+            case 2: rankString = "2ND"; break;
+            case 3: rankString = "3RD"; break;
+
+        }
+
+        entryTransform.Find("PosText").GetComponent<Text>().text = rankString;
+        int score = highscoreEntry.score;
+        entryTransform.Find("ScoreText").GetComponent<Text>().text = score.ToString();
+
+        string name = highscoreEntry.name;
+        entryTransform.Find("NameText").GetComponent<Text>().text = name;
+
+
+        transformList.Add(entryTransform);
+    }
+
+    private void AddHighscoreEntry(int score, string name)
+    {
+        var highscoreEntry = new HighscoreEntry() { score = score, name = name };
+
+        string jsonString = PlayerPrefs.GetString("highscoreTable");
+        var highscores = JsonUtility.FromJson<Highscores>(jsonString);
+
+        highscores.highscoreEntryList.Add(highscoreEntry);
+        string json = JsonUtility.ToJson(highscores);
+        PlayerPrefs.SetString("highscoreTable", json);
+        PlayerPrefs.Save();
+
+    }
+
+
+    private class Highscores
+    {
+        public List<HighscoreEntry> highscoreEntryList;
+    }
+
+    [System.Serializable]
+    private class HighscoreEntry : IComparable<HighscoreEntry>
+    {
+        public int score;
+        public string name;
+
+        public int CompareTo(HighscoreEntry other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+
+            return other.score - score;
+        }
+    }
+}
+
